2cbd18ffb5f91d4e8129c05681840daa2291be3e
juanfer93 <jfpi_0793@hotmail.com>
2025-12-01 11:14:02 -0500

    Add admin status endpoint

--- src/modules/usuarios/usuarios.module.ts
+++ src/modules/usuarios/usuarios.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosService } from './usuarios.service';
-import { UsuariosController } from './usuarios.controller';
+import { UsuariosController, UsersPublicController } from './usuarios.controller';
 import { Usuario } from './entities/usuario.entity';
 import { UsuariosRepository } from './repositories/usuarios.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
-  controllers: [UsuariosController],
+  controllers: [UsuariosController, UsersPublicController],
   providers: [UsuariosService, UsuariosRepository],
   exports: [UsuariosService],
 })
